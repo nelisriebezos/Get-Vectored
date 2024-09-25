f6b27b49a61e1cbef33ad61c80ba087b3ee49d58
nelisriebezos <niels@riezebos.net>
2024-09-25 14:04:33 +0200

    demo working

--- src/main/java/nelisriebezos/getvectored/App.java
+++ src/main/java/nelisriebezos/getvectored/App.java
@@ -1,10 +1,9 @@
-package nelisriebezos.getvectored;
+ package nelisriebezos.getvectored;
 
-/**
- * Hello world!
- */
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    	EmbeddingDemo demo = new EmbeddingDemo();
+    	demo.demoCallEmbeddedFloat();
     }
 }
---
+++ src/main/java/nelisriebezos/getvectored/EmbeddingDemo.java
@@ -0,0 +1,41 @@
+package nelisriebezos.getvectored;
+
+import java.util.Arrays;
+
+import io.github.sashirestela.openai.BaseSimpleOpenAI;
+import io.github.sashirestela.openai.SimpleOpenAI;
+import io.github.sashirestela.openai.domain.embedding.EmbeddingFloat;
+import io.github.sashirestela.openai.domain.embedding.EmbeddingRequest;
+
+public class EmbeddingDemo {
+	private BaseSimpleOpenAI openAI;
+	
+	
+	public EmbeddingDemo() {
+		String apiKey = System.getenv("OPENAI_GETVECTORED_KEY");
+		String organizationId = System.getenv("OPENAI_ORGANIZATION_ID");
+		this.openAI = SimpleOpenAI.builder()
+				.apiKey(apiKey)
+				.organizationId(organizationId)
+				.build();
+	}
+
+
+
+	public void demoCallEmbeddedFloat() {
+		var embeddingRequest = EmbeddingRequest.builder()
+				.model("text-embedding-3-small")
+				.input(Arrays.asList(
+						"shiny sun",
+						"blue sky"
+						))
+				.build();
+        var futureEmbedding = openAI.embeddings().create(embeddingRequest);
+        var embeddingResponse = futureEmbedding.join();
+        embeddingResponse.getData()
+                .stream()
+                .map(EmbeddingFloat::getEmbedding)
+                .forEach(System.out::println);
+		
+	}
+}
