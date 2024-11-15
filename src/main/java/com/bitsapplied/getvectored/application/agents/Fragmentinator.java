f2bd5cf1696ff863ba94bbd7f42b381488a52e77
nelisriebezos <niels@riezebos.net>
2024-11-15 12:14:15 +0100

    fragmentinator system prompt

--- src/main/java/com/bitsapplied/getvectored/application/agents/Fragmentinator.java
+++ src/main/java/com/bitsapplied/getvectored/application/agents/Fragmentinator.java
@@ -1,34 +1,28 @@
 package com.bitsapplied.getvectored.application.agents;
 
-import com.bitsapplied.getvectored.application.agents.exceptions.SettingPromptTemplateException;
 import com.bitsapplied.getvectored.application.services.FileService;
 import com.bitsapplied.getvectored.util.StringUtils;
 import com.bitsapplied.morpheus.core.agent.impl.LLMAgent;
 import com.bitsapplied.morpheus.core.env.Context;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Chunkinator extends LLMAgent {
+public class Fragmentinator extends LLMAgent {
     private final FileService fileService;
     private String promptTemplate;
     private String systemPromptTemplate;
 
-    public Chunkinator(Context context, String identifier, FileService fileService) {
+    public Fragmentinator(Context context, String identifier, FileService fileService) {
         super(context, identifier);
         this.fileService = fileService;
     }
 
     public void setPrompts(String systemPromptTemplate, String promptTemplate) {
-        try {
             Map<String, String> promptTemplateArguments = new HashMap<>();
             Map<String, String> systemPromptTemplateArguments = new HashMap<>();
             this.systemPromptTemplate = StringUtils.replaceArguments(systemPromptTemplate, systemPromptTemplateArguments);
             this.promptTemplate = StringUtils.replaceArguments(promptTemplate, promptTemplateArguments);
-        } catch (IOException e) {
-            throw new SettingPromptTemplateException(e.getMessage());
-        }
     }
 
     @Override
